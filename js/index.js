2201a71dcc1bc6776e0f5085877fe896fb37c326
Gabriel Levi <137122494+GabrielLevisilva@users.noreply.github.com>
2023-09-08 15:32:27 -0300

    v2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-const btnVerificar = document.querySelector('.btn')
-let chances = 10
-btnVerificar.addEventListener('click', verificar)
-
 
 let numerosDitos = []
 
 
+jogar()
+
+function jogar(){
+
+    const btnVerificar = document.querySelector('.btn')
+    btnVerificar.addEventListener('click', verificar)
+let chances = 10
 
 // Gerar um número aleatório entre 1 e 100 (inclusive)
 var numeroSecreto = Math.floor(Math.random() * 100) + 1;
@@ -15,23 +18,26 @@ console.log(numeroSecreto);
 
 const palpitesAnteriores = document.querySelector('.palpites-anteriores')
 const casoErre = document.querySelector('.caso-erre')
-const contadorChances = document.querySelector('.contador-chances')
+const contadorChances = document.querySelector('.chances')
+const numTxt = document.querySelector('#numtxt')
+numTxt.focus()
 
 
 
 function verificar(){
 
-    const numTxt = document.querySelector('#numtxt')
+   
     let num = Number(numTxt.value)
     if(numerosDitos.indexOf(num) !== -1){
         alert('você já escolheu esse numero')
     }
     else{
+        let palpitePassado = document.createElement('p')
 
         if(num < 1 || num > 100){
             alert('ERRO digite um número entre 1 e 100')
         }
-        else{
+        else if(num >= 1 && num <= 100 ){
             if(num == numeroSecreto){
                 numeroSecretoAcertado()
             }
@@ -39,16 +45,16 @@ function verificar(){
                 chances--
                 numerosDitos.push(num)
                 casoErre.innerHTML = `ERROU, ${num} é um número muito BAIXO`
-
+                palpitePassado.style.color = 'red'
+                
             }
             else if(num > numeroSecreto){
                 chances--
                 numerosDitos.push(num)
                 casoErre.innerHTML = `ERROU, ${num} é um número muito ALTO`
-
-                
+                palpitePassado.style.color = 'blue'
             }
-            let palpitePassado = document.createElement('p')
+            
             palpitePassado.innerHTML = num
 
             palpitesAnteriores.appendChild(palpitePassado)
@@ -67,13 +73,31 @@ function verificar(){
 
 const main = document.querySelector('main')
 const ganhou = document.querySelector('.ganhou')
-const localNumCerto = document.querySelector('.num-certo')
-localNumCerto.innerHTML = numeroSecreto
+const localNumCerto = document.querySelectorAll('.num-certo')
+for(let c = 0; c< localNumCerto.length; c++){
+    localNumCerto[c].innerHTML = numeroSecreto
+}
 
+const perdeu = document.querySelector('.perdeu')
+const jogarNovamente = document.querySelector('.jogar-novamente')
+jogarNovamente.addEventListener('click', function(){
+    
+    main.style.display = 'block'
+    perdeu.style.display = 'none'
+    palpitesAnteriores.innerHTML = ''
+    chances = 10
+    casoErre.innerHTML = ''
+    contadorChances.innerHTML = 10
+    numerosDitos = [] 
+    btnVerificar.removeEventListener('click', verificar)
+    jogar()
+    
+})
 
 function chancesAcabaram(){
     alert('suas chances acabaram')
     main.style.display = 'none'
+    perdeu.style.display = 'block'
 }
 
 function numeroSecretoAcertado(){
@@ -82,3 +106,4 @@ function numeroSecretoAcertado(){
 
 }
 
+}
